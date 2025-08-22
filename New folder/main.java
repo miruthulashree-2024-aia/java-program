7f8e3379f073cc983945913ab1e871cacfd0c6b9
miruthulashree-2024-aia <miruthulashrees44@gmail.com>
2025-08-22 13:26:15 +0530

    Update main.java

--- New folder/main.java
+++ New folder/main.java
@@ -1,19 +1,34 @@
+import java.util.Scanner;
+
 class Myclass {
     String name;
     int rollno;
     int age;
-    void displayInfo(){
-        System.out.println("Name:"+ name);
-        System.out.println("Rollno:"+ rollno);
-        System.out.println("Age:"+ age);
+
+    void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Rollno: " + rollno);
+        System.out.println("Age: " + age);
     }
 }
-public class main{
+
+public class main {
     public static void main(String[] args) {
-        Myclass s1=new Myclass();
-        s1.name="sushmitha";
-        s1.rollno=61;
-        s1.age=25;
+        Scanner scanner = new Scanner(System.in);
+
+        Myclass s1 = new Myclass();
+
+        System.out.print("Enter name: ");
+        s1.name = scanner.nextLine();
+
+        System.out.print("Enter roll number: ");
+        s1.rollno = scanner.nextInt();
+
+        System.out.print("Enter age: ");
+        s1.age = scanner.nextInt();
+
         s1.displayInfo();
+
+        scanner.close();
     }
 }
